323f904de92d02e33015590901bcaade1808db9c
Guan Yueming <3079756620@qq.com>
2024-06-11 11:36:03 +0800

    first commit

---
+++ src/components/ExampleDemo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    子组件
+    <ExampleSonDemoVue :visible="visible" @close="changeVisible" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import ExampleSonDemoVue from "./ExampleSonDemo.vue";
+import { ref } from 'vue'
+const visible = ref(true)
+
+const changeVisible = (val) => {
+  visible.value = val
+}
+</script>
+
+<style lang='scss' scoped>
+</style>
---
+++ src/components/ExampleSonDemo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>我是父 {{ visible }}
+
+    <button @click="closeFn">关闭</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  visible: {
+    type: Boolean,
+    default: false
+  }
+})
+
+const emits = defineEmits(['close'])
+
+const closeFn = () => {
+  emits('close', false)
+}
+</script>
+<style lang='scss' scoped>
+</style>
